c9204ebc2314ef53f1e3653c7a8398a804f372f4
Bernd Heidemann <b.heidemann@schule.bremen.de>
2022-06-02 13:17:15 +0200

    Fertig

--- src/emailverwaltung/EmailKontaktDAO.java
+++ src/emailverwaltung/EmailKontaktDAO.java
@@ -4,15 +4,20 @@ import java.sql.*;
 
 public class EmailKontaktDAO {
 
+    // Wir brauchen in jeder Methode immer die URL für die Connection,
+    // daher hier abspeichern
     private String url;
 
+    // Konstruktor
+    // beim Erzeugen der DAO wird das hier aufgerufen
     public EmailKontaktDAO() {
-        try {
             // Treiber laden
+        try {
             Class.forName ("org.sqlite.JDBC");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
         // URL für die Verbindung
         this.url="jdbc:sqlite:emails.db";
     }
@@ -20,27 +25,43 @@ public class EmailKontaktDAO {
     public EmailKontakt select(int id) {
         // Ein erstmal leerer EmailKontakt, in dem die
         // Ergebnisse abgespeichert werden sollen
-        EmailKontakt emailKontakt=new EmailKontakt();
 
         try {
+
             // Verbindung aufbauen
-            Connection conn= DriverManager.getConnection(url);
+            Connection conn = DriverManager.getConnection(url);
+
+            // ab hier --> conn ist die Verbindung zur DB
 
             // SQL-Statement - Fragezeichen als Platzhalter, das füllen wir gleich
-            String sql="select * from Email where id = ?";
+            String sql = "select * from Email where id = ?";
 
             // Statement vorbereiten, mit dem SQL-Befehl
-            PreparedStatement stmt=conn.prepareStatement(sql);
+            // Statement ist das, was wir dann an die Datenbank schicken.
+            // Kapselt den SQL-Befehl
+            PreparedStatement stmt = conn.prepareStatement(sql);
 
             // Tauschen wir das Fragezeichen aus
-            // 1 --> das erste Fragezeichen
-            // 2 --> damit austauschen
+
+            // select * from Email where id = ?
+            // 1 --> das erste Fragezeichen im SQL-Befehl austauschen
+
+            // stmt.setString(1, "Foo") ---> Einen String setzen, inkl. Hochkommata
+            // stmt.setInt(1, 42) --> setzen der Zahl 42, ohne Hochkommata
             stmt.setInt(1, id);
+            // SQL ist jetzt, sofern id 3 --> select * from Email where id = 3
+
+            // Mal mit zwei Fragezeichen
+            // select * from Email where name like ? or id= ?
+            // stmt.setString(1, "%eide%")
+            // stmt.setInt(2, 42)
+            // --->  select * from Email where name like '%eide' or id = 42
+
 
             // Das vorbereitete Statement mit dem SQL abschicken
             // Da kommt ein ResultSet zurück, mit der Ergebnistabelle
             // Das ResultSet ermöglicht den Zugriff aus das Ergebnis des SQL-Befehl
-            ResultSet rs=stmt.executeQuery();
+            ResultSet rs = stmt.executeQuery();
 
             // Problem: Das ResultSet springt zeilenweise (also Zeile für Zeile) durch die Ergebnistabelle
             //          Am Anfang ist quasi keine Zeile geladen, d.h. der Zeiger steht
@@ -49,29 +70,36 @@ public class EmailKontaktDAO {
             rs.next();
 
             // extrahieren aus dem ResultSet rs den Wert in der Spalte nachname
-            String name=rs.getString("nachname");
+            // Das ResultSet ist jetzt eine Zeile, die ungefähr so aufgebaut ist
+
+            // Zeilenüberschriften --->   nachname  | vorname  | email
+            // Inhalt              --->   Heidemann | Bernd    | a@b.de
+
+            String name = rs.getString("nachname");
+
+            EmailKontakt emailKontakt=new EmailKontakt();
+
             // und setzen ihn im EmailKontakt ein
             emailKontakt.setNachname(name);
 
             emailKontakt.setVorname(rs.getString("vorname"));
             emailKontakt.setEmail(rs.getString("email"));
 
-
+            // Ergebnis an die GUI zurückliefern
+            return emailKontakt;
         } catch(Exception e) {
             e.printStackTrace();
         }
-        // Ergebnis an die GUI zurückliefern
-        return emailKontakt;
+        return null;
     }
 
     public EmailKontakt first() {
 
-        Connection conn=null;
         try {
             // Connection herstellen
-            conn= DriverManager.getConnection(this.url);
+            Connection conn= DriverManager.getConnection(this.url);
 
-            // SQL Befehl um den ersten Datensazu zu finden
+            // SQL Befehl um den ersten Datensatz zu finden
             String sql="select * from Email where id=(select min(id) from email)";
 
             // Aus dem SQL-Befehl ein Statement machen
@@ -101,32 +129,121 @@ public class EmailKontaktDAO {
             e.printStackTrace();
         }
 
-
         return null;
     }
 
     public EmailKontakt last() {
+        try {
+            Connection conn= DriverManager.getConnection(this.url);
+            String sql="select * from Email where id=(select max(id) from email)";
+            PreparedStatement stmt=conn.prepareStatement(sql);
+            ResultSet rs=stmt.executeQuery();
+            rs.next();
+            EmailKontakt emailKontakt=new EmailKontakt();
+            emailKontakt.setEmail(rs.getString("email"));
+            emailKontakt.setNachname(rs.getString("nachname"));
+            emailKontakt.setVorname(rs.getString("vorname"));
+            emailKontakt.setId(rs.getInt("id"));
+            conn.close();
+            return emailKontakt;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     public EmailKontakt next(EmailKontakt emailKontakt) {
+        try {
+            Connection conn= DriverManager.getConnection(this.url);
+            String sql="select * from email where id = (select min(id) from email where id > ?)";
+            PreparedStatement stmt=conn.prepareStatement(sql);
+            stmt.setInt(1, emailKontakt.getId());
+            ResultSet rs=stmt.executeQuery();
+            rs.next();
+            EmailKontakt nextEmailKontakt=new EmailKontakt();
+            nextEmailKontakt.setEmail(rs.getString("email"));
+            nextEmailKontakt.setNachname(rs.getString("nachname"));
+            nextEmailKontakt.setVorname(rs.getString("vorname"));
+            nextEmailKontakt.setId(rs.getInt("id"));
+            conn.close();
+            return nextEmailKontakt;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     public EmailKontakt previous(EmailKontakt emailKontakt) {
+        try {
+            Connection conn= DriverManager.getConnection(this.url);
+            String sql="select * from email where id = (select max(id) from email where id < ?)";
+            PreparedStatement stmt=conn.prepareStatement(sql);
+            stmt.setInt(1, emailKontakt.getId());
+            ResultSet rs=stmt.executeQuery();
+            rs.next();
+            EmailKontakt nextEmailKontakt=new EmailKontakt();
+            nextEmailKontakt.setEmail(rs.getString("email"));
+            nextEmailKontakt.setNachname(rs.getString("nachname"));
+            nextEmailKontakt.setVorname(rs.getString("vorname"));
+            nextEmailKontakt.setId(rs.getInt("id"));
+            conn.close();
+            return nextEmailKontakt;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
-    public void delete(EmailKontakt emailKontakt) {
+    public void delete(int id) {
+        try {
 
+            // Connection aufbauen
+            Connection conn =  DriverManager.getConnection(url);
+
+            // SQL-Befehl schreiben
+            String sql="delete from Email where id = ?";
+
+            // Statement erzeugen
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            // ggf. die Fragezeichen im Statement austauschen
+            statement.setInt(1, id);
+
+            // Statement abschicken
+            statement.executeUpdate();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void insert(EmailKontakt emailKontakt) {
+        try {
+            Connection conn=DriverManager.getConnection(url);
+            String sql="insert into Email (vorname, nachname, email) VALUES(?, ?, ?)";
+            PreparedStatement statement=conn.prepareStatement(sql);
+            statement.setString(1, emailKontakt.getVorname());
+            statement.setString(2, emailKontakt.getNachname());
+            statement.setString(3, emailKontakt.getEmail());
+            statement.executeUpdate();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void update(EmailKontakt emailKontakt) {
-
+        try {
+            Connection conn=DriverManager.getConnection(url);
+            String sql="update email set vorname=?, nachname=?, email=? where id = ?";
+            PreparedStatement statement=conn.prepareStatement(sql);
+            statement.setString(1, emailKontakt.getVorname());
+            statement.setString(2, emailKontakt.getNachname());
+            statement.setString(3, emailKontakt.getEmail());
+            statement.setInt(4, emailKontakt.getId());
+            statement.executeUpdate();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
--- src/emailverwaltung/EmailMainComponent.java
+++ src/emailverwaltung/EmailMainComponent.java
@@ -2,8 +2,6 @@ package emailverwaltung;
 
 
 import javax.swing.*;
-import java.awt.event.ActionListener;
-import java.sql.*;
 
 public class EmailMainComponent {
     private JPanel rootPanel;
@@ -14,37 +12,106 @@ public class EmailMainComponent {
     private JButton suchenButton;
     private JButton buttonFirst;
     private JButton buttonBack;
-    private JButton neuButton;
-    private JButton sichernButton;
+    private JButton anlegenButton;
+    private JButton aendernButton;
     private JButton löschenButton;
     private JButton buttonLast;
     private JButton buttonNext;
 
+    // zentral gespeicherte Referenz auf die DAO
     private EmailKontaktDAO dao;
 
     public EmailMainComponent() {
+        // dao erstellen
         this.dao=new EmailKontaktDAO();
+
+        // ActionListener registrieren
+        // ActionListener --> hört zu ob ein Knopf gedrückt wird
         buttonFirst.addActionListener(e -> first());
-        sichernButton.addActionListener(e -> sichern());
+        buttonLast.addActionListener(e -> last());
+        aendernButton.addActionListener(e -> aendern());
         suchenButton.addActionListener(e -> suchen());
+        buttonBack.addActionListener(e -> back());
+        löschenButton.addActionListener(e -> delete());
+        anlegenButton.addActionListener(e -> insert());
+        buttonNext.addActionListener(e -> next());
+        buttonBack.addActionListener(e -> prev());
+    }
+
+    private void next() {
+        EmailKontakt current=new EmailKontakt();
+        current.setId(Integer.parseInt(textFieldId.getText()));
+        EmailKontakt next=this.dao.next(current);
+        textFieldEmail.setText(next.getEmail());
+        textFieldId.setText(Integer.toString(next.getId()));
+        textFieldNachname.setText(next.getNachname());
+        textFieldVorname.setText(next.getVorname());
+    }
+
+    private void prev() {
+        EmailKontakt current=new EmailKontakt();
+        current.setId(Integer.parseInt(textFieldId.getText()));
+        EmailKontakt next=this.dao.previous(current);
+        textFieldEmail.setText(next.getEmail());
+        textFieldId.setText(Integer.toString(next.getId()));
+        textFieldNachname.setText(next.getNachname());
+        textFieldVorname.setText(next.getVorname());
+    }
+
+    private void aendern() {
+        EmailKontakt toUpdate=new EmailKontakt();
+        toUpdate.setEmail(textFieldEmail.getText());
+        toUpdate.setVorname(textFieldVorname.getText());
+        toUpdate.setNachname(textFieldNachname.getText());
+        toUpdate.setId(Integer.parseInt(textFieldId.getText()));
+        dao.insert(toUpdate);
+    }
+
+    private void insert() {
+        EmailKontakt toStore=new EmailKontakt();
+        toStore.setEmail(textFieldEmail.getText());
+        toStore.setVorname(textFieldVorname.getText());
+        toStore.setNachname(textFieldNachname.getText());
+        dao.insert(toStore);
+        last();
+    }
+
+    private void last() {
+        EmailKontakt emailKontakt=this.dao.last();
+        textFieldEmail.setText(emailKontakt.getEmail());
+        textFieldId.setText(Integer.toString(emailKontakt.getId()));
+        textFieldNachname.setText(emailKontakt.getNachname());
+        textFieldVorname.setText(emailKontakt.getVorname());
+    }
+
+    private void delete() {
+        dao.delete(Integer.parseInt(textFieldId.getText()));
+        textFieldEmail.setText("");
+        textFieldId.setText("");
+        textFieldNachname.setText("");
+        textFieldVorname.setText("");
+    }
+
+    private void back() {
     }
 
     private void suchen() {
+        // textFieldId auslesen und direkt zu einem Int konvertieren
         int id=Integer.parseInt(textFieldId.getText());
+
 
         // Wir rufen in der DAO die select-Methode auf
         // die liefert uns einen fertigen EmailKontakt,
         // den wir nur noch anzeigen müssem
         EmailKontakt kontakt=dao.select(id);
 
+        // Email aus dem geladenen EmailKontakt holen und in das Textfeld email setzen
         textFieldEmail.setText(kontakt.getEmail());
         textFieldVorname.setText(kontakt.getVorname());
         textFieldNachname.setText(kontakt.getNachname());
     }
 
-    private void sichern() {
 
-    }
 
 
 
